641f2dc46e21242197a476bb4fc191da965ae01c
archit <archit.baliyan@1mg.com>
2023-07-17 15:44:40 +0530

    commit

--- Booking.rb
+++ Booking.rb
@@ -7,10 +7,10 @@ require 'csv'
  # start of booking class
 
  class  Booking
-    
+
     @@headers = ["Phone_Number", "Start_Date", "End_Date", "Gym_Opted", "Laundry_opted", "food_opted", "Amount"]
     def createBooking(phone_No)
-        
+
         details = []
         details << phone_No
         room = displayRooms
@@ -20,61 +20,61 @@ require 'csv'
         while true
          puts "Enter Start Date"
          startDate = gets.chomp
-       
+
          y, m, d = startDate.split '-'
          if Date.valid_date? y.to_i, m.to_i, d.to_i
             break
-         else  
-            puts "Enter Correct Start Date"  
-         end 
+         else
+            puts "Enter Correct Start Date"
+         end
         end
-        
+
         while true
          puts "Enter End Date"
          endDate = gets.chomp
-         
+
          y, m, d = endDate.split '-'
          if Date.valid_date? y.to_i, m.to_i, d.to_i
             break
-         else  
-            puts "Enter Correct End Date"  
-         end 
+         else
+            puts "Enter Correct End Date"
+         end
         end
-         
-        if Date.parse(startDate) > Date.parse(endDate) 
+
+        if Date.parse(startDate) > Date.parse(endDate)
             puts "Please enter correct Dates"
-        else   
+        else
             difference_in_days = (Date.parse(endDate) - Date.parse(startDate)).to_i
             amount = difference_in_days*amount
             details << startDate
             details << endDate
-            break  
-        end    
-    end 
- 
-        # Asking for Gym Charges 
-        
+            break
+        end
+    end
+
+        # Asking for Gym Charges
+
         gym_opted = "No"
         while true
-        puts "Gym Required ? (Y/N)" 
+        puts "Gym Required ? (Y/N)"
         input = gets.chomp
-        if input == 'Y' || input == 'y' 
+        if input == 'Y' || input == 'y'
             amount = amount + 1000
             gym_opted = "Yes"
             break
         elsif input == 'N' || input == 'n'
             breaks
-        else puts "choose correct options !!"      
-        end    
+        else puts "choose correct options !!"
+        end
         end
 
         # Asking for Laundry Charges
-        
+
         laundry_opted = "No"
         while true
-        puts "Laundry Required ? (Y/N)" 
+        puts "Laundry Required ? (Y/N)"
         input = gets.chomp
-        if input == 'Y' || input == 'y' 
+        if input == 'Y' || input == 'y'
             amount = amount + 1000
             laundry_opted = "Yes"
             break
@@ -85,12 +85,12 @@ require 'csv'
         end
 
         # Asking for Food Charges
-       
+
         food_opted = "No"
         while true
-        puts "Food Required ? (Y/N)" 
+        puts "Food Required ? (Y/N)"
         input = gets.chomp
-        if input == 'Y' || input == 'y' 
+        if input == 'Y' || input == 'y'
             amount = amount + 1500
             food_opted = "Yes"
             break
@@ -103,22 +103,22 @@ require 'csv'
         puts "Total cost : #{amount}"
         puts "Thanks for the Booking, wish you a Happy Stay"
         details << gym_opted
-        details << laundry_opted 
+        details << laundry_opted
         details << food_opted
         details << amount
-        
+
         CSV.open("Booking_data.csv", "a") do |csv|
-           if File.zero?("Booking_data.csv")  
-            csv << @@headers 
-           end 
-            csv << details 
-        end   
+           if File.zero?("Booking_data.csv")
+            csv << @@headers
+           end
+            csv << details
+        end
         # we have to save payment and booking data
         return room
-    end    
+    end
 
-end    
+end
 
-o = Booking.new
-o.createBooking
+
+
 
